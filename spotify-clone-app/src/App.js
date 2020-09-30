e90013ce069a89c6bc54d42fefd80de7b07df739
fadosu <fadosu@ttu.ee>
2020-09-30 05:34:21 +0300

    Initial App Commit

---
+++ spotify-clone-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './Components/Login'
+
+function App() {
+  //BEM Convention
+  return (
+    <div className="app">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ spotify-clone-app/src/Components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Style/Login.css'
+
+const Login = () => {
+    return (
+        <div>
+             {/*Spotify Clone App*/}
+             {/*Login with spotify Button*/}
+             <h1>Login</h1>
+        </div>
+    )
+}
+
+export default Login
